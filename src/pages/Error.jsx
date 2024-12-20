295d174ece3c42c6f7b32be1924444fdaf67f1f1
sai shravan <saishravan384@gmail.com>
2024-12-20 15:45:27 +0530

    feat: Add home link in the error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,10 +1,12 @@
+import { Link } from 'react-router-dom'
 import Navbar from '../components/Navbar'
 
 export default function Error() {
   return (
     <>
       <Navbar />
-      <p>Error</p>
+      <h1>OOPS! An error has occured.</h1>
+      <Link to={'/'}>Go back to home</Link>
     </>
   )
 }
