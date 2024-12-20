64d83e2a4984ef3be2c695e39d6c61546aa4ba7b
sai shravan <saishravan384@gmail.com>
2024-12-20 15:40:33 +0530

    feat: Add basic routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import 'normalize.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import Router from './Router.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Router />
   </StrictMode>
 )
---
+++ src/pages/Example1.jsx
@@ -0,0 +1,7 @@
+export default function Example1() {
+  return (
+    <div>
+      <p>Example 1</p>
+    </div>
+  )
+}
